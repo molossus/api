5a0f8e55f310de8b312ff4a5272aa220975ff701
Kristofer M White <me@kmwhite.net>
2016-07-11 20:38:41 -0500

    Adding goals and tags to data model

--- MAIN.sql
+++ MAIN.sql
@@ -37,7 +37,8 @@ COMMENT ON EXTENSION "uuid-ossp" IS 'UUID Generation Framework';
 CREATE TABLE transaction_statuses (
 	id integer PRIMARY KEY,
 	name character varying NOT NULL CHECK (name <> ''),
-	description text NOT NULL CHECK (description <> '')
+	description text NOT NULL CHECK (description <> ''),
+	active boolean NOT NULL DEFAULT true
 );
 CREATE SEQUENCE IF NOT EXISTS transaction_statuses_id_seq
 	START WITH 1
@@ -47,6 +48,7 @@ CREATE SEQUENCE IF NOT EXISTS transaction_statuses_id_seq
 	NO MAXVALUE
 	CACHE 1;
 ALTER SEQUENCE transaction_statuses_id_seq OWNED BY transaction_statuses.id;
+ALTER TABLE ONLY transaction_statuses ALTER COLUMN id SET DEFAULT nextval('transaction_statuses_id_seq'::regclass);
 
 --
 -- Transactions - Individual Line Item Entry Records
@@ -66,6 +68,46 @@ CREATE SEQUENCE IF NOT EXISTS transactions_id_seq
 	NO MAXVALUE
 	CACHE 1;
 ALTER SEQUENCE transactions_id_seq OWNED BY transactions.id;
+ALTER TABLE ONLY transactions ALTER COLUMN id SET DEFAULT nextval('transactions_id_seq'::regclass);
+
+--
+-- Tags - Categorical Grouping of Saved Funds
+--
+CREATE TABLE tags (
+	id integer PRIMARY KEY,
+	name character varying NOT NULL CHECK (name <> ''),
+	description text NOT NULL CHECK (description <> ''),
+	active boolean NOT NULL DEFAULT true
+);
+CREATE SEQUENCE IF NOT EXISTS tags_id_seq
+	START WITH 1
+	RESTART WITH 1
+	INCREMENT BY 1
+	NO MINVALUE
+	NO MAXVALUE
+	CACHE 1;
+ALTER SEQUENCE tags_id_seq OWNED BY tags.id;
+ALTER TABLE ONLY tags ALTER COLUMN id SET DEFAULT nextval('tags_id_seq'::regclass);
+
+--
+-- Goals -
+--
+CREATE TABLE goals (
+	id integer PRIMARY KEY,
+	name character varying NOT NULL CHECK (name <> ''),
+	description text NOT NULL CHECK (description <> ''),
+	tag_id integer NOT NULL REFERENCES tags (id),
+	due_date date NOT NULL CHECK (due_date >= CURRENT_DATE)
+);
+CREATE SEQUENCE IF NOT EXISTS goals_id_seq
+	START WITH 1
+	RESTART WITH 1
+	INCREMENT BY 1
+	NO MINVALUE
+	NO MAXVALUE
+	CACHE 1;
+ALTER SEQUENCE goals_id_seq OWNED BY goals.id;
+ALTER TABLE ONLY goals ALTER COLUMN id SET DEFAULT nextval('goals_id_seq'::regclass);
 
 -- COMMIT TRANSACTION
 COMMIT;
