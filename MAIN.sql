b4569782d2c930eaba43cafa8c4a4aef7e52bebd
Kristofer M White <me@kmwhite.net>
2016-07-14 23:46:07 -0500

    Switch to UUID table PKs

--- MAIN.sql
+++ MAIN.sql
@@ -35,81 +35,45 @@ COMMENT ON EXTENSION "uuid-ossp" IS 'UUID Generation Framework';
 -- TransactionStatus - State Machine
 --
 CREATE TABLE transaction_statuses (
-	id integer PRIMARY KEY,
+	id uuid default uuid_generate_v4() PRIMARY KEY,
 	name character varying NOT NULL CHECK (name <> ''),
 	description text NOT NULL CHECK (description <> ''),
 	active boolean NOT NULL DEFAULT true
 );
-CREATE SEQUENCE IF NOT EXISTS transaction_statuses_id_seq
-	START WITH 1
-	RESTART WITH 1
-	INCREMENT BY 1
-	NO MINVALUE
-	NO MAXVALUE
-	CACHE 1;
-ALTER SEQUENCE transaction_statuses_id_seq OWNED BY transaction_statuses.id;
-ALTER TABLE ONLY transaction_statuses ALTER COLUMN id SET DEFAULT nextval('transaction_statuses_id_seq'::regclass);
 
 --
 -- Transactions - Individual Line Item Entry Records
 --
 CREATE TYPE transaction_type AS ENUM ('debit', 'credit');
 CREATE TABLE transactions (
-	id integer PRIMARY KEY,
+	id uuid default uuid_generate_v4() PRIMARY KEY,
 	name character varying NOT NULL CHECK (name <> ''),
 	occurred_on date NOT NULL,
-	status_id integer NOT NULL REFERENCES transaction_statuses (id),
+	transaction_status_id uuid NOT NULL REFERENCES transaction_statuses (id),
 	type transaction_type NOT NULL,
 	amount decimal(8,2) NOT NULL
 );
-CREATE SEQUENCE IF NOT EXISTS transactions_id_seq
-	START WITH 10000000
-	RESTART WITH 10000000
-	INCREMENT BY 1
-	NO MINVALUE
-	NO MAXVALUE
-	CACHE 1;
-ALTER SEQUENCE transactions_id_seq OWNED BY transactions.id;
-ALTER TABLE ONLY transactions ALTER COLUMN id SET DEFAULT nextval('transactions_id_seq'::regclass);
 
 --
 -- Tags - Categorical Grouping of Saved Funds
 --
 CREATE TABLE tags (
-	id integer PRIMARY KEY,
+	id uuid default uuid_generate_v4() PRIMARY KEY,
 	name character varying NOT NULL CHECK (name <> ''),
 	description text NOT NULL CHECK (description <> ''),
 	active boolean NOT NULL DEFAULT true
 );
-CREATE SEQUENCE IF NOT EXISTS tags_id_seq
-	START WITH 1
-	RESTART WITH 1
-	INCREMENT BY 1
-	NO MINVALUE
-	NO MAXVALUE
-	CACHE 1;
-ALTER SEQUENCE tags_id_seq OWNED BY tags.id;
-ALTER TABLE ONLY tags ALTER COLUMN id SET DEFAULT nextval('tags_id_seq'::regclass);
 
 --
 -- Goals -
 --
 CREATE TABLE goals (
-	id integer PRIMARY KEY,
+	id uuid default uuid_generate_v4() PRIMARY KEY,
 	name character varying NOT NULL CHECK (name <> ''),
 	description text NOT NULL CHECK (description <> ''),
-	tag_id integer NOT NULL REFERENCES tags (id),
-	due_date date NOT NULL CHECK (due_date >= CURRENT_DATE)
+	due_date date NOT NULL CHECK (due_date >= CURRENT_DATE),
+	tag_id uuid NOT NULL REFERENCES tags (id)
 );
-CREATE SEQUENCE IF NOT EXISTS goals_id_seq
-	START WITH 1
-	RESTART WITH 1
-	INCREMENT BY 1
-	NO MINVALUE
-	NO MAXVALUE
-	CACHE 1;
-ALTER SEQUENCE goals_id_seq OWNED BY goals.id;
-ALTER TABLE ONLY goals ALTER COLUMN id SET DEFAULT nextval('goals_id_seq'::regclass);
 
 -- COMMIT TRANSACTION
 COMMIT;
